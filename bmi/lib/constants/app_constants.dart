73fd5d7dd8fc8dea9f9e01641d6ca7efc39c9429
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2022-05-31 16:14:27 +0600

    Commit BMI 1

---
+++ bmi/lib/constants/app_constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+final Color mainHexColor = HexColor("#4b81ab");
+final Color accentHexColor = HexColor("#3d0016");
---
+++ bmi/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:bmi/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Body Mass Index (BMI) calculator',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
